c4ba5aff64d2ec42635470ecca4bd8ddd08af23f
Devin Khun <lightningowl2004@gmail.com>
2023-06-26 20:54:41 -0700

    Add assignment 2

--- Assignment2/heap.java
+++ Assignment2/heap.java
@@ -1,5 +1,205 @@
 package Assignment2;
 
+import java.util.Arrays;
+
+/**
+ * Heap class for an array implementation of a heap.
+ */
 public class heap {
-    
+    private int[] heap;
+    private int lastIndex;
+    private int swapCount = 0;
+    private boolean integrityOK = false;
+    private static final int DEFAULT_CAPACITY = 25;
+    private static final int MAX_CAPACITY = 1000;
+
+    /**
+     * The constructor to create a heap array with default size.
+     */
+    public heap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * The constructor to create a heap with an initialCapacity.
+     * @param initialCapacity  The initial size of the heap array.
+     */
+    public heap(int initialCapacity) {
+        if (initialCapacity < DEFAULT_CAPACITY) {
+            initialCapacity = DEFAULT_CAPACITY;
+        } else if (initialCapacity > MAX_CAPACITY) {
+            throw new ArrayIndexOutOfBoundsException("The capacity is too large.");
+        }
+        heap = new int[initialCapacity];
+        lastIndex = 0;
+        integrityOK = true;
+    }
+
+    /**
+     * The constructor to create a optimal heap from an array of integers.
+     * @param entries  The array of integers to create a heap of.
+     */
+    public heap(int[] entries) {
+        this(entries.length + 1);
+        lastIndex = entries.length;
+        for (int index = 0; index < entries.length; index++) {
+            heap[index + 1] = entries[index];
+        }
+        for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--) {
+            reheap(rootIndex);
+        }
+    }
+
+    /**
+     * Adds a new entry to the heap array.
+     * @param newEntry  The entry to be added to the heap array.
+     */
+    public void add(int newEntry) {
+        checkIntegrity();
+        int newIndex = lastIndex + 1;
+        int parentIndex = newIndex / 2;
+        while ((parentIndex > 0) && (newEntry > heap[parentIndex])) {
+            heap[newIndex] = heap[parentIndex];
+            newIndex = parentIndex;
+            parentIndex = newIndex / 2;
+            swapCount++;
+        }
+        heap[newIndex] = newEntry;
+        lastIndex++;
+        ensureCapacity();
+    }
+
+    /**
+     * Removes the maximum integer from the heap array.
+     * @return  The removed integer from the heap array.
+     */
+    public int removeMax() {
+        checkIntegrity();
+        int root = 0;
+        if (!isEmpty()) {
+            root = heap[1];
+            heap[1] = heap[lastIndex];
+            lastIndex--;
+            reheap(1);
+        }
+        return root;
+    }
+
+    /**
+     * Returns the maximum integer or root of the heap array.
+     * @return  The integer value of the root in the heap array.
+     */
+    public int getMax() {
+        checkIntegrity();
+        int root = 0;
+        if (!isEmpty()) {
+            root = heap[1];
+        }
+        return root;
+    }
+
+    public int getValue(int index) {
+        return heap[index];
+    }
+
+    /**
+     * Checks if the heap array is empty or not.
+     * @return  True if the heap is empty, or false otherwise.
+     */
+    public boolean isEmpty() {
+        return lastIndex < 1;
+    }
+
+    /**
+     * Retuns the size of the heap array.
+     * @return  The size of the heap array.
+     */
+    public int getSize() {
+        return lastIndex;
+    }
+
+    /**
+     * Returns the reheap count of the reheap operation.
+     * @return  The number of reheap operations.
+     */
+    public int getSwapCount() {
+        return swapCount;
+    }
+
+    /**
+     * Clears and removes the integers from the heap array.
+     */
+    public void clear() {
+        checkIntegrity();
+        while (lastIndex > 0) {
+            heap[lastIndex] = 0;
+            lastIndex--;
+        }
+        lastIndex = 0;
+    }
+
+    /**
+     * Returns an array of the first ten integers of the heap.
+     * @return  The first ten integers of the heap array.
+     */
+    public int[] getFirstTen() {
+        int[] result = new int[10];
+        int[] copy = this.toArray();
+        for (int i = 0; i < 10; i++) {
+            result[i] = copy[i+1];
+        }
+        return result;
+    }
+
+    /**
+     * Returns an array of the heap.
+     * @return  The array version of the heap.
+     */
+    public int[] toArray() {
+        return Arrays.copyOf(heap, heap.length);
+    }
+
+    /**
+     * Reheaps the heap array to meet the criteria of a heap.
+     * @param rootIndex  The root index of the heap array.
+     */
+    private void reheap(int rootIndex) {
+        boolean done = false;
+        int orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+        while(!done && (leftChildIndex <= lastIndex)) {
+            int largerChildIndex = leftChildIndex;
+            int rightChildIndex = leftChildIndex + 1;
+            if ((rightChildIndex <= lastIndex) && (heap[rightChildIndex] > heap[largerChildIndex])) {
+                largerChildIndex = rightChildIndex;
+            }
+            if (orphan < heap[largerChildIndex]) {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+                swapCount++;
+            } else {
+                done = true;
+            }
+        }
+        heap[rootIndex] = orphan;
+    }
+
+    /**
+     * Ensures that the heap array has not reached its capacity, and doubles the size if it is.
+     */
+    private void ensureCapacity() {
+        if (lastIndex + 1 >= heap.length) {
+            heap = Arrays.copyOf(heap, heap.length * 2);
+        }
+    }
+
+    /**
+     * Checks if the heap was intiated correctly.
+     */
+    private void checkIntegrity() {
+        if (!integrityOK) {
+            throw new IllegalStateException("The heap was not properly intialized.");
+        }
+    }
 }
--- Assignment2/testHeap.java
+++ Assignment2/testHeap.java
@@ -1,5 +1,78 @@
 package Assignment2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * TestHeap class to create heaps using the sequential and optimal methods.
+ */
 public class testHeap {
-    
+    public static void main(String[] args) {
+        // Create an array for data entries
+        int[] entries = new int[100];
+
+        // Read in integers from data.txt into entries array
+        try {
+            File data = new File("Assignment2/data.txt");
+            Scanner scanner = new Scanner(data);
+            int index = 0;
+            while (scanner.hasNext()) {
+                entries[index] = scanner.nextInt();
+                index++;
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured when trying to read in input file.");
+        }
+        
+        // Write the output to the output file, output.txt
+        try {
+            // Create output file, output.txt
+            FileWriter output = new FileWriter("Assignment2/output.txt");
+            
+            // Sequential Heap Creation
+            heap heap1 = new heap();
+            for (int entry : entries) {
+                heap1.add(entry);
+            }
+
+            // Optimal Heap Creation
+            heap heap2 = new heap(entries);
+            
+            // Print sequential heap and number of swaps for creation
+            output.write("=====================================================================\n");
+            output.write("Heap built using sequential insertions: " + Arrays.toString(heap1.getFirstTen()) + "\n");
+            output.write("Number of swaps in the heap creation: " + heap1.getSwapCount() + "\n");
+
+            // Remove ten integers from sequential heap
+            for (int i = 0; i < 10; i++) {
+                heap1.removeMax();
+            }
+
+            // Print sequential heap after removal
+            output.write("Heap after 10 removals: " + Arrays.toString(heap1.getFirstTen()) + "\n\n");
+
+            // Print optimal heap and number of swaps for creation
+            output.write("Heap built using optimal method: " + Arrays.toString(heap2.getFirstTen()) + "\n");
+            output.write("Number of swaps in the heap creation: " + heap2.getSwapCount() + "\n");
+
+            // Remove ten integers from optimal heap
+            for (int i = 0; i < 10; i++) {
+                heap2.removeMax();
+            }
+            
+            // Print optimal heap after removal
+            output.write("Heap after 10 removals: " + Arrays.toString(heap2.getFirstTen()) + "\n");
+            output.write("=====================================================================\n");
+
+            output.close();
+        } catch (IOException e) {
+            System.out.println("An error occured when trying to write to output file.");
+            e.printStackTrace();
+        }
+    }
 }
